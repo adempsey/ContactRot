2f33af6dae6876979c2fbc8f7566255e4982e05c
Andrew Dempsey <amd0331@gmail.com>
2017-10-05 17:47:46 -0400

    Remove storyboard

---
+++ ContactRot/ContactListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactListViewController.swift
+//  ContactRot
+//
+//  Created by Andrew Dempsey on 10/5/17.
+//  Copyright © 2017 Dempsey. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
