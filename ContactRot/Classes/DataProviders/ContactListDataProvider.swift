6e93b6e47b89221fdf258865621b170b467c8f3f
Andrew Dempsey <amd0331@gmail.com>
2017-10-06 16:04:51 -0400

    Create contact list data provider

--- ContactRot/Classes/DataProviders/ContactListDataProvider.swift
+++ ContactRot/Classes/DataProviders/ContactListDataProvider.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
+import Contacts
 
 class ContactListDataProvider: NSObject {
 
-    
+    private let keysToFetch: [CNKeyDescriptor] = [CNContactGivenNameKey as CNKeyDescriptor,
+                                                  CNContactFamilyNameKey as CNKeyDescriptor]
+
+    private var contacts: [CNContact] = Array()
+
+    public var count: Int {
+        return self.contacts.count
+    }
+
+    public func retrieve() {
+        let req = CNContactFetchRequest(keysToFetch: self.keysToFetch)
+        try! CNContactStore().enumerateContacts(with: req) {
+            contact, stop in
+            self.contacts.append(contact)
+        }
+    }
+
+    public subscript(row: Int) -> CNContact {
+        get {
+            return self.contacts[row]
+        }
+    }
 
 }
--- ContactRot/ContactListViewController.swift
+++ ContactRot/ContactListViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import SnapKit
 
 class ContactListViewController: UIViewController {
 
+    fileprivate let reuseIdentifier = "contact_list_cell"
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: self.reuseIdentifier)
+        tableView.dataSource = self
+
+        return tableView
+    }()
+
+    fileprivate let dataProvider = ContactListDataProvider()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(self.tableView)
+        self.createConstraints()
+
+        self.dataProvider.retrieve()
+        self.tableView.reloadData()
+    }
+
+    private func createConstraints() {
+        self.tableView.snp.makeConstraints { (make) -> Void in
+            make.edges.equalTo(self.view)
+        }
     }
 
 }
 
+extension ContactListViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataProvider.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier,
+                                                 for: indexPath)
+
+        cell.textLabel?.text = self.dataProvider[indexPath.row].givenName
+
+        return cell
+    }
+    
+}
